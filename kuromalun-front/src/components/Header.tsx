269a01949152c333c17c7425c5b8bca52b3a9b1d
hiromuota166 <otahiromu166@gmail.com>
2024-05-01 23:37:23 +0900

    add:header作った

--- kuromalun-front/src/components/Header.tsx
+++ kuromalun-front/src/components/Header.tsx
@@ -1,12 +1,17 @@
 'use client'
-import { PhoneIcon } from '@chakra-ui/icons'
+import { SettingsIcon } from '@chakra-ui/icons'
 
 const Header = () => {
   return (
-    <>
-      <PhoneIcon />
-      <div>Header</div>
-    </>
+    <header className="shadow-boxOut bg-background flex justify-between">
+      <div>
+        <h1 className="p-4 pt-6 text-3xl text-font">
+          <a href="/" className='text-maincolor'>kuromalun</a>
+        </h1>
+      </div>
+      <div className="items-center flex p-4">
+      </div>
+    </header>
   )
 }
 
