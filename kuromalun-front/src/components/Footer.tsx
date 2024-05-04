1b9e40058595be2c521c194529e2cc92cf428707
hiromuota166 <otahiromu166@gmail.com>
2024-05-04 19:08:47 +0900

    add:フッターにアイコン等を作成

--- kuromalun-front/src/components/Footer.tsx
+++ kuromalun-front/src/components/Footer.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+'use client'
+import { Icon } from '@chakra-ui/icons';
+import { IoHomeOutline } from "react-icons/io5";
 
 const Footer = () => {
   return (
     <div className='fixed inset-x-0 bottom-0 bg-background'>
-      <div className='text-center p-4 bg-gray-100'>
-        Footer
+      <div className='text-center p-4 bg-gray-100 flex justify-center gap-10'>
+        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
+          <Icon as={IoHomeOutline} w={6} h={6}/>
+          <span>ホーム</span>
+        </a>
+        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
+          <Icon as={IoHomeOutline} w={6} h={6}/>
+          <span>ホーム</span>
+        </a>
+        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
+          <Icon as={IoHomeOutline} w={6} h={6}/>
+          <span>ホーム</span>
+        </a>
       </div>
     </div>
   );
