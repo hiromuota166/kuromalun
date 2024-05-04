d2b1cc9c3c10932a42a0dc30203e96b794d6d327
hiromuota166 <otahiromu166@gmail.com>
2024-05-04 19:14:15 +0900

    add:FooterIconコンポーネント作成

--- kuromalun-front/src/components/Footer.tsx
+++ kuromalun-front/src/components/Footer.tsx
@@ -1,23 +1,16 @@
 'use client'
 import { Icon } from '@chakra-ui/icons';
-import { IoHomeOutline } from "react-icons/io5";
+import { IoHomeOutline, IoCreateOutline } from "react-icons/io5";
+import { CiUser } from "react-icons/ci";
+import FooterIcon from './FooterIcon';
 
 const Footer = () => {
   return (
     <div className='fixed inset-x-0 bottom-0 bg-background'>
       <div className='text-center p-4 bg-gray-100 flex justify-center gap-10'>
-        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
-          <Icon as={IoHomeOutline} w={6} h={6}/>
-          <span>ホーム</span>
-        </a>
-        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
-          <Icon as={IoHomeOutline} w={6} h={6}/>
-          <span>ホーム</span>
-        </a>
-        <a href="/" className="text-mainColor flex flex-col items-center mx-4">
-          <Icon as={IoHomeOutline} w={6} h={6}/>
-          <span>ホーム</span>
-        </a>
+        <FooterIcon href='/' text='ホーム' icon={IoHomeOutline} />
+        <FooterIcon href='/create' text='作成' icon={IoCreateOutline} />
+        <FooterIcon href='/user' text='ユーザー' icon={CiUser} />
       </div>
     </div>
   );
