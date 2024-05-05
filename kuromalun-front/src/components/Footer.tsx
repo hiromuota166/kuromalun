6a90f5f891c2bf6b5980aa8dab1f87fffd737adc
hiromuota166 <otahiromu166@gmail.com>
2024-05-06 00:44:52 +0900

    fix:signUpに直しました

--- kuromalun-front/src/components/Footer.tsx
+++ kuromalun-front/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className='fixed inset-x-0 bottom-0 bg-backgroundColor'>
       <div className='text-center p-4 bg-gray-100 flex justify-center gap-10'>
         <FooterIcon href='/' text='ホーム' icon={IoHomeOutline} />
-        <FooterIcon href='/create' text='作成' icon={IoCreateOutline} />
+        <FooterIcon href='/signUp' text='作成' icon={IoCreateOutline} />
         <FooterIcon href='/user' text='ユーザー' icon={CiUser} />
       </div>
     </div>
