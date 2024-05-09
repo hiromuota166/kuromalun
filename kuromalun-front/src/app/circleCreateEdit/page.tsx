6fa00f287d9c3f33b8acdd71b5e45369eb383b6e
hiromuota166 <otahiromu166@gmail.com>
2024-05-10 01:02:05 +0900

    add: icon表示

--- kuromalun-front/src/app/circleCreateEdit/page.tsx
+++ kuromalun-front/src/app/circleCreateEdit/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import { Image } from '@chakra-ui/react'
+import { IoClipboardSharp } from "react-icons/io5";
+import DefaultIcon from '@/components/DefaultIcon';
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='bg-color'>
+      {/* サークル名 */}
+      <div className='bg-backgroundColor'>
+        <h1>
+          <input type='text' placeholder='サークル名' />
+        </h1>
+      </div >
+      {/* 内容 */}
+      <div className='bg-mainColor'>
+        <div>
+          <div>
+            <DefaultIcon icon={IoClipboardSharp} width={4} height={4}/>
+          </div>
+          <textarea placeholder='内容' />
+        </div>
+        {/* バツボタン */}
+        <div>
+          <button aria-label='store'>
+            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'/>
+          </button>
+        </div>
+
+      </div >
+    </div>
   )
 }
 
