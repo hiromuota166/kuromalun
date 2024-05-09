75b074b898ed803a4c77ee90eee519cd63949766
hiromuota166 <otahiromu166@gmail.com>
2024-05-10 06:55:47 +0900

    fix:サイズ修正

--- kuromalun-front/src/components/CircleDetailTextItem.tsx
+++ kuromalun-front/src/components/CircleDetailTextItem.tsx
@@ -12,7 +12,7 @@ interface CircleDetailTextItemProps {
 const CircleDetailTextItem = (props: CircleDetailTextItemProps) => {
   const { icon, width, height, text } = props;
   return (
-    <div className='flex'>
+    <div className='flex mb-4'>
       <DefaultIcon icon={icon} width={width} height={height}/>
       <p className='pl-4'>{text}</p>
     </div>
