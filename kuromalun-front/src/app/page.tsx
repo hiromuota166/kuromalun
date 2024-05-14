3be0597215b010f8f17b80c21c0f37beb94830be
hiromuota166 <otahiromu166@gmail.com>
2024-05-14 20:21:55 +0900

    add:サークル画のスライドショーコンポーネント

--- kuromalun-front/src/app/page.tsx
+++ kuromalun-front/src/app/page.tsx
@@ -1,7 +1,19 @@
+import TopTagLisť from "@/components/TopTagLisť";
+import SlideShow from "@/components/SlideShow";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="">
+      <div className="bg-backgroundColor h-fit">
+        <TopTagLisť />
+      </div>
+      <div className="bg-backgroundColor h-fit">
+        <SlideShow />
+      </div>
+      <div className="bg-backgroundColor h-fit">
+        <h1>Home</h1>
+      </div>
     </div>
+
   );
 }
