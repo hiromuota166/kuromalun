69e1e2c4fdc5db744e558b2f8e50785cf0f580f3
hiromuota166 <otahiromu166@gmail.com>
2024-05-17 19:33:40 +0900

    add:スライドできるタグごとの掲示板コンポーネント

--- kuromalun-front/src/app/page.tsx
+++ kuromalun-front/src/app/page.tsx
@@ -3,17 +3,48 @@ import SlideShow from "@/components/SlideShow";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="pb-24 bg-backgroundColor">
       <div className="bg-backgroundColor h-fit">
         <TopTagLisť />
       </div>
       <div className="bg-backgroundColor h-fit">
         <SlideShow />
       </div>
-      <div className="bg-backgroundColor h-fit">
-        <h1>Home</h1>
+      <div className="bg-backgroundColor h-fit p-4">
+        <div className="text-mainColor">
+          <h1 className="text-xl">大学公認部活動</h1>
+        </div>
+        <div className="w-full overflow-hidden">
+          <div className="flex space-x-4 overflow-x-scroll py-4">
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/hackathon.PNG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/tennisCircle.JPG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+          </div>
+        </div>
+        <div className="text-mainColor">
+          <h1 className="text-xl">大学公認同好会</h1>
+        </div>
+        <div className="w-full overflow-hidden">
+          <div className="flex space-x-4 overflow-x-scroll py-4">
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/hackathon.PNG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/tennisCircle.JPG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+          </div>
+        </div>
+        <div className="text-mainColor">
+          <h1 className="text-xl">大学非公認サークル</h1>
+        </div>
+        <div className="w-full overflow-hidden">
+          <div className="flex space-x-4 overflow-x-scroll py-4">
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/hackathon.PNG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/tennisCircle.JPG" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+            <img className="rounded-lg flex-shrink-0 w-64 h-40" src="/haikei.png" alt="背景を挿入" />
+          </div>
+        </div>
       </div>
     </div>
-
   );
 }
