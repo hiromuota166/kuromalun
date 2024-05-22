00c9e4f27e16f5a9fb1406c878d5409e8c501607
hiromuota166 <otahiromu166@gmail.com>
2024-05-22 21:14:18 +0900

    add:ダミーテキストを見られてもいいように変更

--- kuromalun-front/src/app/set/page.tsx
+++ kuromalun-front/src/app/set/page.tsx
@@ -3,9 +3,9 @@ import SettingTag from "@/components/SettingTag"
 
 const page = () => {
   return (
-    <div className=' h-[calc(100vh-80px)] bg-backgroundColor'>
-      <SettingTag title='製作者について' content='ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト'/>
-      <SettingTag title='このアプリについて' content='ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト'/>
+    <div className='h-[calc(100vh-56px)] flex flex-col bg-backgroundColor text-mainColor mt-8'>
+      <SettingTag title='製作者について' content='watnowの春プロジェクトにて運営中'/>
+      <SettingTag title='このアプリについて' content='watnowのメンバー3人で制作されています'/>
     </div>
   )
 }
