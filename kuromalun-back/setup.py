3c1976a3bfe608b060dca3bd1e1becbca2d4ea6b
Chiro358 <chiro6083@gmail.com>
2024-05-28 11:37:07 +0900

    setup更新

--- kuromalun-back/setup.py
+++ kuromalun-back/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="kuromalun-back",
     version="0.1",
-    packages=find_packages(where="src/mymodules"),
-    package_dir={"": "src/mymodules"},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
