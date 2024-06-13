18b5b02c782a0facc0ecead00c714201d5099adc
hiromuota166 <otahiromu166@gmail.com>
2024-06-13 17:48:24 +0900

    add:loginページ

---
+++ kuromalun-front/src/app/login/actions.ts
@@ -0,0 +1,34 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+/**
+ * ログイン
+ *
+ * ログインが成功した場合はトップページへリダイレクトする。
+ * ログインに失敗した場合はエラーページへリダイレクトする。
+ *
+ * @param formData - フォームから受け取ったデータ
+ * @returns void
+ */
+export async function login(formData: FormData) {
+  const supabase = createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.error('Login Error:', error)
+    redirect(`/error?message=${encodeURIComponent(error.message)}`)
+    return
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
