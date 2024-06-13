6cdd998813ff77b92a5bfdb9536609b765a8761d
hiromuota166 <otahiromu166@gmail.com>
2024-06-13 17:12:42 +0900

    fix:errorページ変えた

--- kuromalun-front/src/app/error/page.tsx
+++ kuromalun-front/src/app/error/page.tsx
@@ -1,17 +1,9 @@
-'use client'
+import React from 'react'
 
-import { useRouter } from 'next/router'
-
-const ErrorPage = () => {
-  const router = useRouter()
-  const { message } = router.query
-
+const page = () => {
   return (
-    <main>
-      <h1>Error</h1>
-      <p>{message ? decodeURIComponent(message as string) : 'An unexpected error occurred.'}</p>
-    </main>
+    <div>error</div>
   )
 }
 
-export default ErrorPage
+export default page
