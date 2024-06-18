903dd660737140038d2fb2cdd55c5f11e64b5d26
hiromuota166 <otahiromu166@gmail.com>
2024-06-19 01:22:09 +0900

    add:ログイン中にメアドを出力

--- kuromalun-front/src/components/Navigation.tsx
+++ kuromalun-front/src/components/Navigation.tsx
@@ -1,15 +1,52 @@
-'use client'
-import React from 'react'
-import { usePathname } from 'next/navigation'
-import SettingIcon from './SettingIcon'
-import UserIcon from './UserIcon'
+'use client';
+import React, { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import SettingIcon from './SettingIcon';
+import UserIcon from './UserIcon';
+import { supabase } from '../utils/supabase';
 
 const Navigation: React.FC = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
+  const router = useRouter();
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        setUserEmail(user.email ?? null);
+      }
+    };
+
+    fetchUser();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN' && session) {
+        setUserEmail(session.user.email ?? null);
+      } else if (event === 'SIGNED_OUT') {
+        setUserEmail(null);
+      }
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
 
   const getLinkClasses = (path: string): string => {
-    return pathname === path ? 'py-3 px-4 block font-bold border-b-2 text-black' : 'py-3 px-4 block'
-  }
+    return pathname === path ? 'py-3 px-4 block font-bold border-b-2 text-black' : 'py-3 px-4 block';
+  };
+
+  const handleCreateClick = async (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    const { data: { user } } = await supabase.auth.getUser();
+    if (user) {
+      router.push('/circleCreateEdit');
+    } else {
+      alert('ログインしてください');
+      router.push('/login');
+    }
+  };
 
   return (
     <div>
@@ -19,16 +56,17 @@ const Navigation: React.FC = () => {
             <a href="/" className={getLinkClasses('/')}>ホーム</a>
           </li>
           <li className='justify-center'>
-            <a href="/circleCreateEdit" className={getLinkClasses('/circleCreateEdit')}>作成</a>
+            <a href="/circleCreateEdit" onClick={handleCreateClick} className={getLinkClasses('/circleCreateEdit')}>作成</a>
           </li>
         </ul>
-        <ul className=''>
-          <UserIcon/>
+        <ul className='flex items-center'>
+          {userEmail && <li className='mr-4'>{userEmail}</li>}
+          <UserIcon />
         </ul>
       </nav>
       <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
