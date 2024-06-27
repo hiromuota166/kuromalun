91849ae6652986538f619263193e03cfb153baba
hiromuota166 <otahiromu166@gmail.com>
2024-06-28 01:12:45 +0900

    add:main画面の見た目改修

--- kuromalun-front/src/app/page.tsx
+++ kuromalun-front/src/app/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react';
 import { supabase } from '@/utils/supabase';
 import { DetailModal } from '@/components/DetailModal';
 import { useDisclosure } from '@chakra-ui/hooks';
+import { Divider } from '@chakra-ui/react'
+import UserIcon from '@/components/UserIcon';
 
 interface Circle {
   uid: string;
@@ -53,22 +55,32 @@ const Page = () => {
       <h1 className="text-center text-2xl font-bold my-4">サークル一覧</h1>
       <div className="flex flex-wrap mx-3">
         {circles.map(circle => (
-          <div key={circle.uid} className="w-1/4 p-2">
+          <div key={circle.uid} className="w-full p-2">
             <div
               onClick={() => handleCircleClick(circle)}
-              className="cursor-pointer rounded-lg overflow-hidden flex flex-col items-center"
+              className="cursor-pointer rounded-2xl overflow-hidden flex flex-col items-center border relative"
             >
-              <div className="w-24 h-24 rounded-2xl overflow-hidden">
+              <div className="w-full h-48 rounded-2xl overflow-hidden relative md:h-96">
                 <img
                   src={circle.circlesImageId}
                   alt={circle.name}
                   className="w-full h-full object-cover rounded-2xl"
                 />
               </div>
-              <p className="w-20 text-center mt-2 truncate">{circle.name}</p>
+              <div className='px-4 w-full h-24 text-center'>
+                <div className="truncate h-1/2 items-center justify-start flex">
+                  <p className="truncate text-xl">{circle.activity}</p>
+                </div>
+                <div className=''>
+                  <Divider orientation='horizontal' width='100%' />
+                </div>
+                <div className="truncate h-1/2 items-center justify-start flex">
+                  <UserIcon w={32} h={32}/>
+                  <p className="truncate pl-2">{circle.name}</p>
+                </div>
+              </div>
             </div>
           </div>
-
         ))}
       </div>
       <DetailModal circle={selectedCircle} isOpen={isOpen} onClose={onClose} />
